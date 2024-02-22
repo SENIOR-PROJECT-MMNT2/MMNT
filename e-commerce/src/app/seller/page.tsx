c7d6737b4be35f1c3028547eb2b88e09916b4afc
takwach5 <channoufitakwa09@gmail.com>
2024-02-22 10:23:37 +0100

    desing dashbord seller

---
+++ e-commerce/src/app/seller/page.tsx
@@ -0,0 +1,268 @@
+import link from "next/link";
+import { SlTrash } from "react-icons/sl";
+import { SlPencil } from "react-icons/sl";
+import { Interface } from "readline";
+export default function(){
+
+interface Dashboord{
+    
+
+}
+    
+
+
+    return(
+        <div>
+           <div style={{display:'flex',width:'100%'}}>
+
+           <div >
+            <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+          <div className="mb-2 p-4">
+            <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">Dashboord</h5>
+          </div>
+          <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+            <div role="button"  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+              <div className="grid place-items-center mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                  <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+              Product
+            </div>
+            <div role="button"  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+              <div className="grid place-items-center mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                  <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+              AddProduct
+            </div>
+            <div role="button"  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+              <div className="grid place-items-center mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                  <path fill-rule="evenodd" d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z" clip-rule="evenodd"></path>
+                </svg>
+              </div>Category
+            </div>
+    
+            <div role="button"  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+              <div className="grid place-items-center mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>Customer
+            </div>
+            <div role="button"  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+              <div className="grid place-items-center mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-5 w-5">
+                  <path fill-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v9a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM6.166 5.106a.75.75 0 010 1.06 8.25 8.25 0 1011.668 0 .75.75 0 111.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 011.06 0z" clip-rule="evenodd"></path>
+                </svg>
+              </div>Log Out
+            </div>
+          </nav>
+        </div>
+
+        </div>
+
+        <div>
+  
+              
+<div className="container max-w-5xl px-50 mx-auto sm:px-33" style={{marginLeft:70}}>
+    <div className="py-8">
+        <div className="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8  ">
+            <div>
+                <table className="min-w-full leading-normal w-4/5">
+                    <thead>
+                        <tr>
+                            <th scope="col" className=" px-5 py-3 text-sm  font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200 ">
+                                Product
+                            </th>
+                            <th scope="col" className="px-5 py-3 text-sm font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                                description
+                            </th>
+                            <th scope="col" className="px-5 py-3 text-sm font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                                Category
+                            </th>
+                            <th scope="col" className="px-5 py-3 text-sm font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                                Stock
+                            </th>
+                            <th scope="col" className="px-5 py-3 text-sm font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                                Price
+                            </th>
+                            <th scope="col" className="px-5 py-3 text-sm font-sans font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                                Action
+                        
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200 w-80">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <a href="#" className="relative block">
+                                            <img alt="profil" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrwDb-GnUwMOydrERFTg8CLDZqotSdwQ3cBbaG44d3lg&s" className="mx-auto object-cover rounded-full h-20 w-20 "/>
+                                        </a>
+                                    </div>
+                                    <div className="ml-3">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                        phone
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                best phone of the year
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    electronic
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    5
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    50$
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200 w-80">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                    <SlPencil />
+                                    </div>
+                                    <div className="ml-3">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                        <SlTrash />
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                          
+                        </tr>
+                        {/* <tr>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <a href="#" className="relative block">
+                                            <img alt="profil" src="/images/person/9.jpg" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+                                        </a>
+                                    </div>
+                                    <div className="ml-3">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                            Marcus coco
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    Designer
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    01/10/2012
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
+                                    <span aria-hidden="true" className="absolute inset-0 bg-green-200 rounded-full opacity-50">
+                                    </span>
+                                    <span className="relative">
+                                        active
+                                    </span>
+                                </span>
+                            </td>
+                        </tr> */}
+                        {/* <tr> */}
+                            {/* <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <a href="#" className="relative block">
+                                            <img alt="profil" src="/images/person/10.jpg" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+                                        </a>
+                                    </div>
+                                    <div className="ml-3">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                            Ecric marc
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    Developer
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    02/10/2018
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
+                                    <span aria-hidden="true" className="absolute inset-0 bg-green-200 rounded-full opacity-50">
+                                    </span>
+                                    <span className="relative">
+                                        active
+                                    </span>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <a href="#" className="relative block">
+                                            <img alt="profil" src="/images/person/6.jpg" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+                                        </a>
+                                    </div>
+                                    <div className="ml-3">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                            Julien Huger
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    User
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    23/09/2010
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
+                                    <span aria-hidden="true" className="absolute inset-0 bg-green-200 rounded-full opacity-50">
+                                    </span>
+                                    <span className="relative">
+                                        active
+                                    </span>
+                                </span>
+                            </td>
+                        </tr> */}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+        </div>
+           </div>
+        </div>
+        
+        
+        
+            
+        
+    )
+}
