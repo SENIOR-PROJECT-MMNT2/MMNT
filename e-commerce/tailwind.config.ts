bf955fb44620672501dd7997e68c3e1bacc7c076
takwach5 <channoufitakwa09@gmail.com>
2024-02-22 18:42:53 +0100

    90% desgin finsh seller

--- e-commerce/tailwind.config.ts
+++ e-commerce/tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite/**/*.js",
     'node_modules/flowbite-react/lib/esm/**/*.js',
+  
   ],
   theme: {
     extend: {
